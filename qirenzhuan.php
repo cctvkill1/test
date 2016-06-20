72de0b22a6dff19bce6a6efa0d59d7328e4080e0
田聪 <1097422313@qq.com>
2016-06-20 11:58:14 +0800

    速度好慢

--- qirenzhuan.php
+++ qirenzhuan.php
@@ -1,4 +1,4 @@
- <?php 
+ <?php  
  //七人转 
 
  $pagestartime=microtime(); 
@@ -29,8 +29,8 @@
  		} 
  		//记录对手次数
  	}
- 	$countR = count($result)-1;
- 	for ($x=0; $x < count($result); $x++) {
+ 	$countR = 10;
+ 	for ($x=0; $x < 10; $x++) {
  		$x1 = $result[$x][0];
  		$x2 = $result[$x][1];
  		$x3 = $result[$x][2];
@@ -42,6 +42,7 @@
  			$tArr[$x3*10+$x4][$x2]++;  
  		}
  	}
+
  	// var_dump($tArr);echo "<br>";
 	//上面生成了10.5对阵
 	//再增加3.5个对阵 
@@ -85,7 +86,7 @@
  				$tArr[$x3*10+$x4][$x2]++;  
  			}
  		}
- 	}
+ 	} 
  	$isOk = true;
  	for ($i=0; $i < $count*2; $i++) { 
  		for ($j=0; $j < 4; $j++) { 
@@ -99,6 +100,7 @@
  		$result = recursion($arr,$count); 
  		return $result;
  	}else{ 
+ 		//一个组合的单个对手出现一次
  		for ($x=0; $x < count($result); $x++) {
  			$x1 = $result[$x][0];
  			$x2 = $result[$x][1];
@@ -115,6 +117,7 @@
  				// $testAll[$x4]++;
  			}
  		}
+ 		// 这个判断有点多余
  		// foreach ($testAll as $key => $value) {
  		// 	foreach ($value as $k => $v) { 
  		// 		if($v!=8){
@@ -143,6 +146,7 @@
  		// }
  	}
  }
+ //测试得到更多的数组
  function getAll($arr,$count,$i,$allR=array()){
  	if($i>15){
  		return $allR;
@@ -153,6 +157,97 @@
  		$i++;
  	}
  	return	getAll($arr,$count,$i,$allR);
+ }
+
+//输出最后的对阵数组（每个人不能连续出现3次）
+ function endResult($arr,$count){
+ 	$testAll = array();
+ 	for ($i=1; $i <8 ; $i++) {
+ 		$testAll[$i] = 0;
+ 	}
+
+ 	$result = recursion($arr,$count);  
+ 	for ($i=0; $i < $count*2; $i++) {  
+ 		$x1 = $result[$i][0];
+ 		$x2 = $result[$i][1];
+ 		$x3 = $result[$i][2];
+ 		$x4 = $result[$i][3];  
+ 		$flag = 1;
+ 		if($testAll[$x1]>=2) 
+ 			$flag=0;
+ 		if($testAll[$x2]>=2) 
+ 			$flag=0;
+ 		if($testAll[$x3]>=2)
+ 			$flag=0;
+ 		if($testAll[$x4]>=2) 
+ 			$flag=0;
+		// var_dump($result[$i]);
+	 	// var_dump($testAll);
+	 	// print_r($result[$i]);
+ 		if(!$flag){
+ 		// echo "error ";
+ 			for ($x=$i+1; $x < $count*2; $x++) { 
+ 				$flag1 = $flag2 = $flag3 = $flag4 = 1;
+ 				$xx1 = $result[$x][0];
+ 				$xx2 = $result[$x][1];
+ 				$xx3 = $result[$x][2];
+ 				$xx4 = $result[$x][3];  
+ 				if($testAll[$xx1]>=2) 
+ 					$flag1=0;
+ 				if($testAll[$xx2]>=2) 
+ 					$flag2=0;
+ 				if($testAll[$xx3]>=2)
+ 					$flag3=0;
+ 				if($testAll[$xx4]>=2) 
+ 					$flag4=0;
+ 				if($flag1&&$flag2&&$flag3&&$flag4){		
+ 					$temp = $result[$i];
+ 					$result[$i] = $result[$x];
+ 					$result[$x] = $temp ;   
+ 					$flag = 1;
+ 					break; 
+ 				}
+ 			}
+ 			if(!$flag){
+ 				$isReturn = 1;
+ 				break;
+ 			}
+ 		}
+
+ 		// var_dump($result[$i]);
+ 		$x1 = $result[$i][0];
+ 		$x2 = $result[$i][1];
+ 		$x3 = $result[$i][2];
+ 		$x4 = $result[$i][3];  
+ 		$testAll[$x1]++;
+ 		$testAll[$x2]++;
+ 		$testAll[$x3]++;
+ 		$testAll[$x4]++; 
+ 		$testAll[$x1]=$testAll[$x1]>=3?0:$testAll[$x1]; 
+ 		$testAll[$x2]=$testAll[$x2]>=3?0:$testAll[$x2]; 
+ 		$testAll[$x3]=$testAll[$x3]>=3?0:$testAll[$x3]; 
+ 		$testAll[$x4]=$testAll[$x4]>=3?0:$testAll[$x4]; 
+ 		for ($j=0; $j <8 ; $j++) {
+ 			if($j!=$x1&&$j!=$x2&&$j!=$x3&&$j!=$x4) 
+ 				$testAll[$j] = 0; 		
+ 		}
+ 		// print_r($testAll);
+ 	}
+ 	if($isReturn)
+ 		$result = endResult($arr,$count);
+ 	return $result;
+ }
+
+//运气好20MS 平均2000MS
+ $result = recursion($arr,$count); 
+ //太慢咯 运气好30多秒 运气不好超时
+ // $result = endResult($arr,$count);
+
+ for ($i=0; $i < $count*2; $i++) {  
+ 	for ($j=0; $j < 4; $j++) {  
+ 		echo $result[$i][$j].' ';
+ 	}  
+ 	echo "<br>";
  }
 
  // $allR = getAll($arr,$count,0); 
@@ -166,105 +261,15 @@
  // 	}
  // 	echo "=============================<br>";
  // }
-
- $testAll = array();
- for ($i=1; $i <8 ; $i++) { 
- 	$testAll[$i] = 0;
- }
- $result = recursion($arr,$count); 
- //每个人不能连续出现3次
- for ($i=0; $i < $count*2; $i++) {  
- 	$x1 = $result[$i][0];
- 	$x2 = $result[$i][1];
- 	$x3 = $result[$i][2];
- 	$x4 = $result[$i][3];  
- 	$flag = 1;
- 	if($testAll[$x1]>=2) 
- 		$flag=0;
- 	if($testAll[$x2]>=2) 
- 		$flag=0;
- 	if($testAll[$x3]>=2)
- 		$flag=0;
- 	if($testAll[$x4]>=2) 
- 		$flag=0;
- 		// var_dump($result[$i]);
- 	// var_dump($testAll);
- 	// print_r($result[$i]);
- 	if(!$flag){
- 		// echo "error ";
- 		$flag = 1;
- 		for ($x=$i+1; $x < $count*2; $x++) { 
- 			$flag1 = $flag2 = $flag3 = $flag4 = 1;
- 			$xx1 = $result[$x][0];
- 			$xx2 = $result[$x][1];
- 			$xx3 = $result[$x][2];
- 			$xx4 = $result[$x][3];  
- 			if($testAll[$xx1]>=2) 
- 				$flag1=0;
- 			if($testAll[$xx2]>=2) 
- 				$flag2=0;
- 			if($testAll[$xx3]>=2)
- 				$flag3=0;
- 			if($testAll[$xx4]>=2) 
- 				$flag4=0;
- 			if($flag1&&$flag2&&$flag3&&$flag4){		
- 				$temp = $result[$i];
- 				$result[$i] = $result[$x];
- 				$result[$x] = $temp ;  
- 				// echo "change ";
- 				break; 
-
- 			} 
-
- 		}
- 	} 
- 	// var_dump($result[$i]);
- 	$x1 = $result[$i][0];
- 	$x2 = $result[$i][1];
- 	$x3 = $result[$i][2];
- 	$x4 = $result[$i][3];  
- 	$testAll[$x1]++;
- 	$testAll[$x2]++;
- 	$testAll[$x3]++;
- 	$testAll[$x4]++; 
- 	$testAll[$x1]=$testAll[$x1]>=3?0:$testAll[$x1]; 
- 	$testAll[$x2]=$testAll[$x2]>=3?0:$testAll[$x2]; 
- 	$testAll[$x3]=$testAll[$x3]>=3?0:$testAll[$x3]; 
- 	$testAll[$x4]=$testAll[$x4]>=3?0:$testAll[$x4]; 
- 	for ($j=0; $j <8 ; $j++) {
- 		if($j!=$x1&&$j!=$x2&&$j!=$x3&&$j!=$x4) 
- 			$testAll[$j] = 0; 		
- 	}
- 	// print_r($testAll);
- 	for ($j=0; $j < 4; $j++) {  
- 		echo $result[$i][$j].' ';
- 	}  
- 	echo "<br>";
- }
-/*
-4 6  1 5 
-1 7  2 3 
-4 7  3 5 
-2 4  1 6 
-6 7  2 5 
-3 7  1 5 
-1 2  3 4 
-2 6  4 5 
-5 6  1 3 
-1 4  2 7 
-5 7  3 6 
-4 6  2 3 
-1 2  5 7 
-6 7  3 4 
-*/
+ 
 
 
 
- $pageendtime = microtime(); 
- $starttime   = explode(" ",$pagestartime); 
- $endtime     = explode(" ",$pageendtime); 
- $totaltime   = $endtime[0]-$starttime[0]+$endtime[1]-$starttime[1]; 
- $timecost 	 = sprintf("%s",$totaltime*1000);  
- echo "<br>运行时间: $timecost 毫秒<br>";   
+$pageendtime = microtime(); 
+$starttime   = explode(" ",$pagestartime); 
+$endtime     = explode(" ",$pageendtime); 
+$totaltime   = $endtime[0]-$starttime[0]+$endtime[1]-$starttime[1]; 
+$timecost 	 = sprintf("%s",$totaltime*1000);  
+echo "<br>运行时间: $timecost 毫秒<br>";   
  // echo "<br>平均时间: ".($timecost/10)." 毫秒<br>"; 
- ?>
+?>
